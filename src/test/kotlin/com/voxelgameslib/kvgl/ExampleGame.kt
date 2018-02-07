9e431c9f54f2fda0e51b85fa7200816bcf6064ad
Sxtanna <ranald.taylor@gmail.com>
2018-02-07 09:13:24 -0500

    Forgot the game info

--- src/test/kotlin/com/voxelgameslib/kvgl/ExampleGame.kt
+++ src/test/kotlin/com/voxelgameslib/kvgl/ExampleGame.kt
@@ -3,9 +3,11 @@ package com.voxelgameslib.kvgl
 import com.voxelgameslib.voxelgameslib.feature.features.TeamSelectFeature
 import com.voxelgameslib.voxelgameslib.game.AbstractGame
 import com.voxelgameslib.voxelgameslib.game.GameDefinition
+import com.voxelgameslib.voxelgameslib.game.GameInfo
 import com.voxelgameslib.voxelgameslib.phase.phases.GracePhase
 import com.voxelgameslib.voxelgameslib.phase.phases.LobbyWithVotePhase
 
+@GameInfo(name = "Example", author = "Sxtanna", version = "v1.0", description = "Example description")
 class ExampleGame : AbstractGame(ExamplePlugin.GAMEMODE) {
 
     override fun initGameFromModule() {
